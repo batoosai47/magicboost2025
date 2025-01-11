2582205dea83ce0b9e2704cb7dba618aebb0985d
batoosai <baaka@hotmail.fr>
2025-01-11 12:52:40 +0100

    Setup Firebase Hosting and workflows

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MagicBoost',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bienvenue sur MagicBoost'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, world!',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
